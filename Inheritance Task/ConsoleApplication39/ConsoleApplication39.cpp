d39a939b5fa3f4d8cee52a087588568437ded224
İslam Salamzadə <islam.salamzade.191@gmail.com>
2022-11-04 01:35:12 +0400

    Inheritance Task

---
+++ Inheritance Task/ConsoleApplication39/ConsoleApplication39.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// Single inheritance
+
+class Ata {
+
+};
+
+class Ogul : Ata {
+
+};
+
+
+// Multi-level inheritance
+
+class Ata {
+
+};
+
+class Ogul : Ata {
+
+};
+
+class Neve : Ogul {
+
+};
+
+// Multiple inheritance 
+
+class Ata {
+
+};
+
+class Ana {
+
+};
+
+class Ogul : Ana, Ata {
+
+};
+
+// Hierarchy
+
+class Ata {
+
+};
+
+class Ogul : Ata {
+
+};
+
+class Ogul2 : Ata {
+
+};
+
+
+
+
