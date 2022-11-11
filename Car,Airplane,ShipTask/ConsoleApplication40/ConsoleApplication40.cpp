ae9f75481aa2895e5bbf7df2b99a7e30c4b8b416
İslam Salamzadə <76969781+isiii555@users.noreply.github.com>
2022-11-11 21:34:33 +0400

    Update ConsoleApplication40.cpp

--- Car,Airplane,ShipTask/ConsoleApplication40/ConsoleApplication40.cpp
+++ Car,Airplane,ShipTask/ConsoleApplication40/ConsoleApplication40.cpp
@@ -13,11 +13,6 @@ class Engine {
 public:
 
     Engine() {
-
-        engineNo = 0;
-        company = "";
-        volume = 0;
-
     }
 
     Engine(int engineNo, string company, double volume) {
@@ -64,232 +59,117 @@ public:
     }
 };
 
-class Car{
-
+class Vehicle {
+public:
+    static int staticId;
     int id;
     string model;
     string vendor;
-    bool hasSpoiler;
     Engine engine;
 
-public:
+    Vehicle(string _vendor, string _model, Engine _engine) {
 
-    Car(int id, string model, string vendor, int engineNo, string company, double volume, bool hasSpoiler) {
-
-        engine.SetEngineNo(engineNo);
-        engine.SetCompany(company);
-        engine.SetVolume(volume);
-        this->id = id;
-        this->model = model;
-        this->vendor = vendor;
-        this->hasSpoiler = hasSpoiler;
+        id = staticId++;
+        vendor = _vendor;
+        model = _model;
+        engine = _engine;
 
     }
 
-    void Show() {
-
+    virtual void Show() {
         cout << "Id : " << id
             << "\nModel : " << model
-            << "\nVendor : " << vendor
-            << "\nSpoiler : " << hasSpoiler << endl;
-            engine.Show();
+            << "\nVendor : " << vendor << endl;
+        engine.Show();
     }
 
-    Car& operator=(Car& oth) {
+};
 
-        id = oth.id;
-        model = oth.model;
-        vendor = oth.vendor;
-        hasSpoiler = oth.hasSpoiler;
-        engine.SetEngineNo(oth.engine.GetEngineNo());
-        engine.SetCompany(oth.engine.GetCompany());
-        engine.SetVolume(oth.engine.GetVolume());
-        return *this;
+int Vehicle::staticId = 1;
+
+class Car : public Vehicle {
+public:
+
+    bool hasSpoiler;
+
+    Car(string model, string vendor, Engine engine, bool hasSpoiler) : Vehicle(vendor,model,engine) {
+        this->hasSpoiler = hasSpoiler;
     }
 
-    friend ostream& operator<<(ostream& out, Car& oth) {
+    void Show() override {
 
-        cout << "Id : " << oth.id
-            << "\nModel : " << oth.model
-            << "\nVendor : " << oth.vendor
-            << "\nSpoiler : " << oth.hasSpoiler << endl;
-        oth.engine.Show();
-        return out;
+        
+        cout << "*********Car*********" << endl;
+        Vehicle::Show();
+        cout << "Spoiler : " << (hasSpoiler ? "var" : "yox" ) << endl;
 
     }
 };
 
-class Ship {
-
-    int id;
-    string model;
-    string vendor;
-    bool hasSail;
-    Engine engine; 
-
+class Ship : public Vehicle {
 public:
 
-    Ship(int id, string model, string vendor, int engineNo, string company, double volume, bool hasSail) {
+    bool hasSail;
 
-        engine.SetEngineNo(engineNo);
-        engine.SetCompany(company);
-        engine.SetVolume(volume);
-        this->id = id;
-        this->model = model;
-        this->vendor = vendor;
+    Ship(bool hasSail) : Vehicle(vendor, model, engine) {
+
         this->hasSail = hasSail;
 
     }
 
-    void Show() {
+    void Show() override {
 
-        cout << "Id : " << id
-            << "\nModel : " << model
-            << "\nVendor : " << vendor
-            << "\nSail : " << hasSail << endl;
-        engine.Show();
-    }
-
-    Ship& operator=(Ship& oth) {
-
-        id = oth.id;
-        model = oth.model;
-        vendor = oth.vendor;
-        hasSail = oth.hasSail;
-        engine.SetEngineNo(oth.engine.GetEngineNo());
-        engine.SetCompany(oth.engine.GetCompany());
-        engine.SetVolume(oth.engine.GetVolume());
-        return *this;
-    }
-
-    friend ostream& operator<<(ostream& out, Ship& oth) {
-
-        cout << "Id : " << oth.id
-            << "\nModel : " << oth.model
-            << "\nVendor : " << oth.vendor
-            << "\nSail : " << oth.hasSail << endl;
-        oth.engine.Show();
-        return out;
+        cout << "*********Ship*********" << endl;
+        Vehicle::Show();
+        cout << "Spoiler : " << (hasSail ? "var" : "yox") << endl;
 
     }
+
 };
 
-class Airplane {
+class Airplane : public Vehicle {
 
-    int id;
-    string model;
-    string vendor;
-    Engine engine;
+
     int passengerCapacity;
     int engineCount;
 
 public:
 
-    Airplane(int id, string model, string vendor, int engineNo, string company, double volume,int passenger,int count) {
+    Airplane(int passenger, int count) : Vehicle(vendor, model, engine) {
 
-        engine.SetEngineNo(engineNo);
-        engine.SetCompany(company);
-        engine.SetVolume(volume);
-        this->id = id;
-        this->model = model;
-        this->vendor = vendor;
         passengerCapacity = passenger;
         engineCount = count;
 
     }
 
-    void Show() {
+    void Show() override {
 
-        cout << "Id : " << id
-            << "\nModel : " << model
-            << "\nVendor : " << vendor
-            << "\nPassenger capacity : " << passengerCapacity
-            << "\nEngine count : " << engineCount << endl;
-        engine.Show();
+        cout << "*********Airplane*********" << endl;
+        Vehicle::Show();
+        cout << "Passenger Capacity : " << passengerCapacity
+            << "Engine Count : " << engineCount << endl;
     }
 
-    Airplane& operator=(Airplane& oth) {
-
-        id = oth.id;
-        model = oth.model;
-        vendor = oth.vendor;
-        passengerCapacity = oth.passengerCapacity;
-        engineCount = oth.engineCount;
-        engine.SetEngineNo(oth.engine.GetEngineNo());
-        engine.SetCompany(oth.engine.GetCompany());
-        engine.SetVolume(oth.engine.GetVolume());
-        return *this;
-
-    }
-
-    friend ostream& operator<<(ostream& out, Airplane& oth) {
-
-        cout << "Id : " << oth.id
-            << "\nModel : " << oth.model
-            << "\nVendor : " << oth.vendor
-            << "\nPassenger capacity : " << oth.passengerCapacity
-            << "\nEngine count : " << oth.engineCount << endl;
-        oth.engine.Show();
-        return out;
-
-    }
-};
-
-template<class T>
-class Stack {
-
-	T* arr;
-	int capacity;
-	int top;
-
-public:
-
-	Stack(const int size) {
-		capacity = size;
-		arr = new T[capacity];
-		top = -1;
-	}
-	
-	bool IsFull() { return capacity - 1 == top; }
-	bool IsEmpty() { return top == -1; }
-
-	void Push(T value) {
-		assert(!IsFull() && "Stack overflow");
-		arr[++top] = value;
-	}
-
-	void Pop() {
-		assert(!IsEmpty() && "Stack is empty");
-		top--;
-	}
-
-	T Top(){ 
-		assert(!IsEmpty() && "Stack is empty");
-		return arr[top];
-	}
-
-	~Stack() { delete[] arr; }
+    
 };
 
 class VehicleDepo {
 public:
-    stack<Car> cars;
-    stack<Airplane> airplanes;
-    stack<Ship> ships;
+    stack<Vehicle*> vehicles;
+
+    void AddVehicle(Vehicle* vehicle) {
+
+        vehicles.push(vehicle);
+
+    }
 
     void ShowAllVehicle() {
-        while (!cars.empty()) {
-            cout << cars.top() << endl;
-            cars.pop();
+
+        while (!vehicles.empty()) {
+            vehicles.top()->Show();
+            vehicles.pop();
         }
-        while (!airplanes.empty()) {
-            cout << airplanes.top() << endl;
-            airplanes.pop();
-        }
-        while (!ships.empty()) {
-            cout << ships.top() << endl;
-            ships.pop();
-        }
+
     }
 
 };
@@ -297,14 +177,11 @@ public:
 int main()
 {
 
-    Car bmw(1, "f30", "bmw", 2, "bmw", 2.3, 1);
-    Airplane bayraktar(2,"bayraktar", "tb2", 2, "bayraktar", 1021,1,1);
-    Ship yaxta(3, "islam", "isi", 3, "isii555", 2323, 1);
+    Vehicle* Garage(new Car("Lada", "2107", Engine(2, "VAZ", 2.3), 1));
     VehicleDepo depo;
-    depo.cars.push(bmw);
-    depo.airplanes.push(bayraktar);
-    depo.ships.push(yaxta);
+
+    depo.AddVehicle(Garage);
     depo.ShowAllVehicle();
 
-}
 
+}
